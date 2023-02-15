a488247834c839f7305dd8e07060d7c88b2a6d7c
yuromanov <gaggy.rom@gmail.com>
2023-02-15 19:34:17 +0300

    feat: implemented unit tests for mappers

---
+++ app/src/test/java/com/romanov/spring_task/mapper/QuoteMapperTest.java
@@ -0,0 +1,42 @@
+package com.romanov.spring_task.mapper;
+
+import com.romanov.spring_task.mapper.impl.QuoteMapperImpl;
+import com.romanov.spring_task.model.dto.QuoteOutput;
+import com.romanov.spring_task.model.entity.QuoteEntity;
+import com.romanov.spring_task.model.entity.UserEntity;
+import com.romanov.spring_task.model.entity.VoteEntity;
+import com.romanov.spring_task.model.enumurated.Content;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class QuoteMapperTest {
+    private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d/MM/yyyy");
+
+    private QuoteMapper mapper = new QuoteMapperImpl();
+
+    @Test
+    void shouldConvertEntity() {
+        QuoteOutput expected = new QuoteOutput(1L,
+                Content.UPVOTE.name(),
+                LocalDate.now().format(FORMATTER),
+                "user",
+                "vote");
+        UserEntity userEntity = new UserEntity();
+        userEntity.setName("user");
+        VoteEntity voteEntity = new VoteEntity();
+        voteEntity.setName("vote");
+        QuoteEntity entity = new QuoteEntity();
+        entity.setId(1L);
+        entity.setContent(Content.UPVOTE);
+        entity.setUpdateDate(LocalDate.now());
+        entity.setUser(userEntity);
+        entity.setVote(voteEntity);
+
+        QuoteOutput actual = mapper.convert(entity);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
